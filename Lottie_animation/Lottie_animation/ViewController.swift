1b1658e8621a33c9c29c7ffb865eebc1255f90a4
Bharani <bharani@ideas2it.com>
2020-03-27 11:26:24 +0530

    Animation using lottie

---
+++ Lottie_animation/Lottie_animation/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Lottie_animation
+//
+//  Created by Bharani on 27/03/20.
+//  Copyright © 2020 bharani. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var animationView: AnimationView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func startAnimation(animation: String) {
+        let animation = Animation.named(animation)
+        animationView.animation = animation
+        animationView.loopMode = .playOnce
+        animationView.play()
+    }
+    @IBAction func startFireWork(_ sender: UIButton) {
+        self.startAnimation(animation: "1801-fireworks")
+    }
+    @IBAction func startBubble(_ sender: UIButton) {
+        self.startAnimation(animation: "94-soda-loader")
+    }
+    @IBAction func sayHi(_ sender: UIButton) {
+        print("Hi")
+    }
+}
